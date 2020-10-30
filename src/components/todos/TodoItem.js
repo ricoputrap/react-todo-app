4054bf08051ede49b540e7d7bd4b03ab03d7c36f
rico.putra <rico.putra@outlook.co.id>
2020-10-30 08:29:38 +0700

    Set default checked on todo item based on completed state

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import "./style/todo-item.css";
-import pencilIcon from '../../img/pencil-icon.svg';
+// import pencilIcon from '../../img/pencil-icon.svg';
 import trashIcon from '../../img/trash-icon.svg';
 // import PropTypes from 'prop-types';
 
@@ -20,17 +20,17 @@ class TodoItem extends React.Component {
     
 
     render() {
-        const { id, title } = this.props.todo;
+        const { id, title, completed } = this.props.todo;
         return (
             <div className="item">
                 <div className="item__content">
-                    <input type="checkbox" onChange={ this.props.markComplete.bind(this, id) } />
+                    <input type="checkbox" onChange={ this.props.markComplete.bind(this, id) } defaultChecked={ completed } />
                     <p style={ this.completedStyle() }>{ title }</p>
                 </div>
                 <div>
-                    <button className="btn btn--icon btn--tosca" >
+                    {/* <button className="btn btn--icon btn--tosca" >
                         <img src={ pencilIcon } alt="edit-icon" onClick={ this.editTodo }/>
-                    </button>
+                    </button> */}
                     <button className="btn btn--icon btn--red">
                         <img src={ trashIcon } alt="del-icon" onClick={ this.props.delTodo.bind(this, id) }/>
                     </button>
